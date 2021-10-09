6a98b6d60897b8ec484542ed20f8796a3f80c623
Rafael Juárez <rafalejy@gmail.com>
2021-10-09 16:31:04 -0600

    cambio url servidor back

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router'
 })
 export class ServiciosService {
 
-    public URL = 'http://34.125.203.249:3000';
+    public URL = 'http://34.125.203.2490';
 
     constructor(private http: HttpClient, private router: Router) { }
 
